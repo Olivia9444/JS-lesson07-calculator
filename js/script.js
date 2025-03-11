8f99bb824a1f51c770667035b7e20c04a3bdd369
Olivia Boone <144468291+Olivia9444@users.noreply.github.com>
2025-03-10 19:00:47 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// Your JS code here
+var subTypeElement = document.querySelector("subscription");
+var subDurationElemnt = document.querySelector("Duration");
+var result = document.querySelector(".result");
+var subType = "basic";
+var subDuration = 1;
+
+SubTypeElement.addEventListener("change", function (e) {
+    subType= e.Target.value;
+    console.log(subTypeElement)
+
+    subDurationElement.addEventListener ("change ", function (e) {
+    subType= e.Target.value;
+    console.log (SubTypeElemt)
+
+    subDurationElement.addEventListener ("Result", function (e) {
+    subType= e.Target.value;
+    console.log (subTypeElemnt)
+        }
+    }
+}
